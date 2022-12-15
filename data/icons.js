935b825a001f308a75b422cb60f1ae627b78dc84
Richard Folea <Folea3@gmail.com>
2022-12-15 06:46:42 -0500

    content updates

--- data/icons.js
+++ data/icons.js
@@ -1,18 +1,33 @@
 import defaulticon from "../assets/images/icons/medium/spell_fire_fire.jpg"
-import warrioricon from "../assets/images/icons/medium/class_warrior.jpg"
-import mageicon from "../assets/images/icons/medium/class_mage.jpg"
-import necromancericon from "../assets/images/icons/medium/class_warlock.jpg"
-import paladinicon from "../assets/images/icons/medium/class_paladin.jpg"
-import rogueicon from "../assets/images/icons/medium/class_rogue.jpg"
-import rangericon from "../assets/images/icons/medium/class_hunter.jpg"
+import warrioricon from "../assets/images/icons/class_warrior.jpg"
+import mageicon from "../assets/images/icons/class_mage.jpg"
+import rangericon from "../assets/images/icons/class_ranger.jpg"
 
 import treebg from "../assets/images/art/llama-king-2.png"
+
+
+// llama/default skills
+import kickicon from "../assets/images/skills/llama/kick.jpg"
+import spiticon from "../assets/images/skills/llama/spit.jpg"
+import healicon from "../assets/images/skills/llama/heal.jpg"
+import diginhoovesicon from "../assets/images/skills/llama/dig-in-hooves.jpg"
+import sharpenhoovesicon from "../assets/images/skills/llama/sharpenhooves.jpg"
+
+
+/* import necromancericon from "../assets/images/icons/medium/class_warlock.jpg"
+import paladinicon from "../assets/images/icons/medium/class_paladin.jpg"
+import rogueicon from "../assets/images/icons/medium/class_rogue.jpg" */
+
 
 export const defaultIcon = defaulticon
 export const warriorIcon = warrioricon
 export const mageIcon = mageicon
-export const necromancerIcon = necromancericon
-export const paladinIcon = paladinicon
-export const rogueIcon = rogueicon
 export const rangerIcon = rangericon
 export const treeBg = treebg
+
+// llama/default skills
+export const kickIcon = kickicon
+export const spitIcon = spiticon
+export const healIcon = healicon
+export const digInHoovesIcon = diginhoovesicon
+export const sharpenHoovesIcon = sharpenhoovesicon
