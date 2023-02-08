d9f987645379aab903fb3a20ea94874fb5e8b0b6
Aelof3 <Folea3@gmail.com>
2023-02-08 04:44:24 +0000

    Deploying to gh-pages from @ LLAMARPG/skills-calculator@7863810e926a080585921c9730fbace3946fe232 🚀

---
+++ _next/static/yn5R9yxuwEgSgT6Abr_4g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/955-d778a254e4a45c36.js","static/chunks/pages/index-d9b8385f49a50a1c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
